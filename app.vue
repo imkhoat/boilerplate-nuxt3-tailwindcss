1e48f121327ae877a87d229976d50c26a16757ad
imkhoat <khoatbui.idea@gmail.com>
2022-01-07 17:20:19 +0700

    feat: add tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="p-4">
+    <button
+      class="ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/10 rounded-full py-1 px-3 xl:flex items-center hover:bg-sky-400/20"
+    >
+      Hello world
+    </button>
   </div>
 </template>
+
+<script lang="ts" setup>
+import "@/assets/css/main.css";
+</script>
